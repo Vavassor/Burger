8aa1fe58364c8322f83bb0d97d7e520d43094ce5
Andrew Dawson <dawso.andrew@gmail.com>
2019-02-28 22:57:16 -0500

    Adds credential passing through an environment variable.

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.DATABASE_URL || {
   host: "localhost",
   port: 3306,
   user: "root",
