41a536dc857f01d1c39d7238de6b7baa1a8cc502
Andrew Dawson <dawso.andrew@gmail.com>
2019-02-28 23:00:31 -0500

    Try changing the environment variable name?

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection(process.env.DATABASE_URL || {
+const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL || {
   host: "localhost",
   port: 3306,
   user: "root",
